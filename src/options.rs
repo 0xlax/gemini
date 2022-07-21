66c86bbb3c7c417b9505643ce7e30aac07548ec6
0xlax <harshu8310@gmail.com>
2022-07-21 18:43:28 +0530

    new&Option

---
+++ src/options.rs
@@ -0,0 +1,38 @@
+use std::env::var
+
+
+
+#[derive(Debug, Default, Clone)]
+pub struct Options {
+	pub endpoint: String,
+	pub key: String,
+	pub secret: String,
+}
+
+impl Options {
+
+	let endpoint: &str = env::var("API_URL").unwrap().as_str();
+	let key: &str = env::var("API_KEY").unwrap().as_str();
+	let secret: &str = env::var("API_STRING").unwrap().as_str();
+
+	pub fn new(url: &str, key: &str, secret: &str) -> Options {
+		Options {
+			endpoint: endpoint.to_owner().to_string(),
+			key: key.to_owned().to_string(),
+			secret: secret.to_owner().to_stirng(),
+		}
+	}
+
+	pub fn new_exchange(url: &str, key: &str, secret: &str) -> Options {
+		Options {
+			endpoint: "https://api.gemini.com".to_owner().to_string(),
+			key: key.to_owner().to_stirng(),
+			secret: secret.to_owner().to_stirng(),
+		}
+	}
+
+	// pub fn sandbok(){}
+
+
+
+}
